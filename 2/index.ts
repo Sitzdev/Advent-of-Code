571e4e3ad0bbf073eafaf32f0db890fecfe383dc
sitzdev <lucassitz@gmail.com>
2024-12-03 02:38:14 +0100

    solved aoc_2

---
+++ 2/index.ts
@@ -0,0 +1,118 @@
+const fs = require("node:fs/promises");
+async function aoc2_1() {
+  try {
+    const data = await fs.readFile("./input.txt", { encoding: "utf8" });
+
+    let reports: Array<Array<number>> = [];
+
+    for (const report of data.split("\n")) {
+      const current = report.split(" ");
+      let currentArray: Array<number> = [];
+      for (const num of current) {
+        const next = parseInt(num);
+        if (!isNaN(next)) {
+          currentArray.push(next);
+          continue;
+        }
+        throw new Error("Not a number");
+      }
+      reports.push(currentArray);
+    }
+
+    let numberOfValidReports = 0;
+
+    for (const report of reports) {
+      let direction = 0; // 0 = init 1 = up 2 = down
+      let valid = true;
+      for (let i = 1; i < report.length; i++) {
+        const distance = report[i] - report[i - 1];
+
+        //if the direction changes, the report is invalid
+        const currentDirection = distance > 0 ? 1 : 2;
+        if (direction !== 0 && currentDirection !== direction) {
+          valid = false;
+          break;
+        }
+        direction = currentDirection;
+        const absoluteDistance = Math.abs(distance);
+        //when the distance is greater than 3 or 0, the report is invalid
+        if (absoluteDistance > 3 || absoluteDistance === 0) {
+          valid = false;
+          break;
+        }
+      }
+      numberOfValidReports += valid ? 1 : 0;
+    }
+
+    console.log(numberOfValidReports);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+
+async function aoc2_2() {
+  try {
+    const data = await fs.readFile("./input.txt", { encoding: "utf8" });
+
+    let reports: Array<Array<number>> = [];
+
+    for (const report of data.split("\n")) {
+      const current = report.split(" ");
+      let currentArray: Array<number> = [];
+      for (const num of current) {
+        const next = parseInt(num);
+        if (!isNaN(next)) {
+          currentArray.push(next);
+          continue;
+        }
+        throw new Error("Not a number");
+      }
+      reports.push(currentArray);
+    }
+
+    let numberOfValidReports = 0;
+
+    for (const report of reports) {
+      if (isSafe(report)) {
+        numberOfValidReports++;
+      } else {
+        for (let i = 0; i < report.length; i++) {
+          const dampened = [...report];
+          dampened.splice(i, 1);
+          if (isSafe(dampened)) {
+            numberOfValidReports++;
+            break;
+          }
+        }
+      }
+    }
+    console.log(numberOfValidReports);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const isSafe = (report: Array<number>) => {
+    let direction = 0;
+    for (let i = 1; i < report.length; i++) {
+      const distance = report[i] - report[i - 1];
+  
+      // if the direction changes, the report is invalid
+      const currentDirection = distance > 0 ? 1 : 2;
+      if (direction !== 0 && currentDirection !== direction) {
+        return false;
+      }
+      direction = currentDirection;
+      const absoluteDistance = Math.abs(distance);
+      // when the distance is greater than 3 or 0, the report is invalid
+      if (absoluteDistance > 3 || absoluteDistance === 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+  
+
+aoc2_1();
+aoc2_2();
